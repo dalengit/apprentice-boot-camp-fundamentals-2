ecdc4ec8ea3e9bf3f2a98b47342383c3bd825593
Dalen Chan <Dalen.Chan@brighthr.com>
2021-12-13 11:43:24 +0000

    Added first test and Score method

--- exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingGameTest.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingGameTest.cs
@@ -1,13 +1,29 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace McrDigital.Bootcamp1.Checkout.Tests
 {
     public class BowlingGameTests
     {
         [Fact]
-        public void RenameMe()
+        public void Bowl_returns_zero()
         {
+            var game = new BowlingGame();
+            var bowls = new Array [
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0];
 
+            var result = game.Score(bowls);
+
+            Assert.Equal(0, result);
         }
     }
 }
---
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/BowlingGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McrDigital.Bootcamp1.Checkout
+{
+    public class BowlingGame
+    {
+        public int Score(Array bowls)
+        {
+            return 0;
+        }
+    }
+}

12aa83986078d3c534264a0dbdf914711b673519
Dalen Chan <Dalen.Chan@brighthr.com>
2021-12-13 12:16:25 +0000

    Added test for correct score

--- exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingGameTest.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingGameTest.cs
@@ -5,11 +5,13 @@ namespace McrDigital.Bootcamp1.Checkout.Tests
 {
     public class BowlingGameTests
     {
+        private readonly BowlingGame _game = new BowlingGame();
+
         [Fact]
         public void Bowl_returns_zero()
         {
-            var game = new BowlingGame();
-            var bowls = new Array [
+            var bowls = new int[] 
+            {
                 0, 0,
                 0, 0,
                 0, 0,
@@ -19,11 +21,35 @@ namespace McrDigital.Bootcamp1.Checkout.Tests
                 0, 0,
                 0, 0,
                 0, 0,
-                0, 0];
-
-            var result = game.Score(bowls);
+                0, 0
+            };
+            
+            var result = _game.Score(bowls);
 
             Assert.Equal(0, result);
+        }
+
+        [Fact]
+
+        public void Bowl_returns_correct_score()
+        {
+            var bowls = new int [] 
+            {
+               1, 1,
+               1, 1,
+               1, 1,
+               1, 1,
+               1, 1,
+               1, 1,
+               1, 1,
+               1, 1,
+               1, 1,
+               1, 1
+            };
+
+            var result = _game.Score(bowls);
+
+            Assert.Equal(20, result);
         }
     }
 }
--- exercises/dotnet/McrDigital.Bootcamp1.Checkout/BowlingGame.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/BowlingGame.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Text;
 
 namespace McrDigital.Bootcamp1.Checkout
 {
     public class BowlingGame
     {
-        public int Score(Array bowls)
+        public int Score(int[] bowls)
         {
-            return 0;
+            return bowls.Sum();
         }
     }
 }

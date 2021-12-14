41c15f196de5bb770fca8da34d9cebfeb71a0206
Dalen Chan <Dalen.Chan@brighthr.com>
2021-12-14 11:08:04 +0000

    Name Refactor and added tests

--- exercises/dotnet/McrDigital.Bootcamp1.Checkout/Thingy.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/Thingy.cs
@@ -4,37 +4,37 @@ namespace apprentice_bootcamp_fundamentals_2
 {
     public class Thingy
     {
-        private int foo;
-        private int bar;
-        private int foobar = new int[] { 0, 0, 0, 0, 0 }.Length;
+        private int _countsUpTo100;
+        private int _countsUpToThree;
+        private int _countsDownFromFive = new int[] { 0, 0, 0, 0, 0 }.Length;
 
-        public string DoTheThing()
+        public string StartFizzBuzz()
         {
-            string s = "";
-            for (; foo < Byte.MaxValue - 155; foo++) s += b(foo) + " ";
-            return s.Substring(0, s.Length - 1);
+            string result = "";
+            for (; _countsUpTo100 < Byte.MaxValue - 155; _countsUpTo100++) result += FizzBuzzValue(_countsUpTo100) + " ";
+            return result.Substring(0, result.Length - 1);
         }
 
-        private string b(int foo)
+        private string FizzBuzzValue(int foo)
         {
-            bar++;
-            foobar--;
+            _countsUpToThree++;
+            _countsDownFromFive--;
 
-            string s = bar == 0b11 || foobar == 0 ? "" : (foo + 1).ToString();
-            if (bar == 0b11) s += BarFunction();
-            if (foobar == 0) s += FooFunction();
-            return s;
+            string result = _countsUpToThree == 0b11 || _countsDownFromFive == 0 ? "" : (foo + 1).ToString();
+            if (_countsUpToThree == 0b11) result += IsFizz();
+            if (_countsDownFromFive == 0) result += IsBuzz();
+            return result;
         }
 
-        private string FooFunction()
+        private string IsBuzz()
         {
-            foobar = new int[] { 0, 0, 0, 0, 0 }.Length;
+            _countsDownFromFive = new int[] { 0, 0, 0, 0, 0 }.Length;
             return DataTypeConverter.ParseHexBinary("42757a7a");
         }
 
-        private string BarFunction()
+        private string IsFizz()
         {
-            bar = 0;
+            _countsUpToThree = 0;
             return DataTypeConverter.ParseHexBinary("46697a7a");
         }
     }
